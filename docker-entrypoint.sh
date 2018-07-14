fc1509ab39d91181b9de6d4ab7adace8c6634a4f
wes <wes@home.local>
2018-07-14 08:53:49 -0500

    Updating to allow env vars + rancher metadata

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export HOSTNAME=$(curl -s rancher-metadata/latest/self/host/name | cut -d'.' -f1)
-export CONTAINER_NAME=$(curl -s rancher-metadata/latest/self/container/name)
+export HOSTNAME=${NODE_NAME:-$(curl -s rancher-metadata/latest/self/host/name | cut -d'.' -f1)}
+export CONTAINER_NAME=${POD_NAME:-$(curl -s rancher-metadata/latest/self/container/name)}
 export DOCKER_FQDN="$CONTAINER_NAME.$HOSTNAME"
 
 env
